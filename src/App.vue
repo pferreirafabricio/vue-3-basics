de9a734c91f385db47e16905f2e4e1f5bc895b00
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2020-12-09 21:44:08 -0300

    feat: add basic menu structure

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Menu />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Menu from './components/Menu.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld,
+    Menu,
   },
 });
 </script>
 
 <style>
+@import '/css/all.css';
+@import './styles/main.css';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Menu.vue
@@ -0,0 +1,50 @@
+<template>
+  <section>
+    <nav class="menu">
+      {{ count }}
+      <a href="#">
+        <i class="fas fa-search"></i>
+      </a>
+      <a href="#">
+        <i class="fas fa-plus-circle"></i>
+      </a>
+      <a href="#">
+        <i class="fas fa-user-circle"></i>
+      </a>
+    </nav>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'Menu',
+  setup() {
+    const count = ref<number>(0);
+
+    return {
+      count,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.menu {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 20%;
+  margin: 0 auto;
+}
+
+.menu a {
+  color: var(--dark);
+  font-size: var(--font-extra-large);
+}
+
+.menu a:hover {
+  text-shadow: 0 4px 8px var(--dark-alpha);
+}
+</style>
